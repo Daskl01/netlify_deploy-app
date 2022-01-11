0739b6dae0481ad9a56c98ac814084db355b7bd5
Daskl01 <aboubackrskl@gmail.com>
2022-01-11 19:02:43 +0000

    End projects

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FaRegCopyright } from "react-icons/fa";
+
+const footer = () => {
+    return (
+        <div className="footer bg-primary text-center py-3 text-white   ">
+            <h1 className="h6">Designed By @ Daskl</h1>
+        </div>
+    )
+}
+
+export default footer;
